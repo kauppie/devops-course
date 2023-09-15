74ec8651db914b5290d95645d6cc968c29298810
Elias Kauppi <elkku.kauppi@gmail.com>
2023-09-15 14:05:08 +0300

    Refactor service 2

--- service2/src/main.rs
+++ service2/src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     fs,
     io::Write,
-    net::SocketAddr,
+    net::{Ipv4Addr, SocketAddr},
     sync::{Arc, Mutex},
     time::Duration,
 };
@@ -24,41 +24,45 @@ async fn main() -> Result<(), StdError> {
     let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
 
     let shared_state = Arc::new(Mutex::new((log_file, Some(shutdown_tx))));
-    let router = Router::new().route("/", routing::post(handler).with_state(shared_state));
+    let router = Router::new().route("/", routing::post(request_handler).with_state(shared_state));
+    let socket_address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8000));
 
     // Wait 2 seconds before starting the server.
     tokio::time::sleep(Duration::from_secs(2)).await;
 
-    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+    axum::Server::bind(&socket_address)
         .serve(router.into_make_service_with_connect_info::<SocketAddr>())
         .with_graceful_shutdown(async {
             let _ = shutdown_rx.await;
         })
         .await
-        .expect("starting server should succeed");
+        .expect("failed to start or run the service");
 
     Ok(())
 }
 
-async fn handler(
+/// Handler of incoming POST requests at root address.
+async fn request_handler(
     State(state): State<Arc<Mutex<(fs::File, Option<oneshot::Sender<()>>)>>>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     data: String,
 ) {
     let mut guard = state.lock().unwrap();
 
+    // Stop service on STOP signal.
     if data == "STOP" {
         let shutdown_tx = &mut guard.1;
 
         if let Some(shutdown_tx) = shutdown_tx.take() {
             shutdown_tx
                 .send(())
-                .expect("oneshot channel should be succeed");
+                .expect("failed to send shutdown signal");
         }
     } else {
         let file = &mut guard.0;
 
         let log_line = format!("{data} {addr}\n");
-        file.write_all(log_line.as_bytes()).unwrap();
+        file.write_all(log_line.as_bytes())
+            .expect("failed to write to log file");
     }
 }
